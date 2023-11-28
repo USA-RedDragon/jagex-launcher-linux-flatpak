e80b64187d1d0993a61d49cb1c0f70fb27fde597
Jacob McSwain <jacob.a.mcswain@gmail.com>
2023-11-27 22:24:12 -0600

    build: change how we install

--- build.sh
+++ build.sh
@@ -47,8 +47,9 @@ if [[ ${DOSIGN} -eq 1 ]]; then
     GPG_ARGS="--gpg-sign=7ADE1CA57A2E2272"
 fi
 
-flatpak-builder ${REPO_ARGS} ${GPG_ARGS} --install --default-branch=stable --user --ccache --force-clean out com.jagex.Launcher.yaml
+flatpak-builder ${REPO_ARGS} ${GPG_ARGS} --default-branch=stable --user --ccache --force-clean out com.jagex.Launcher.yaml
 if [[ ${DOSIGN} -eq 0 ]]; then
     flatpak build-export repo out stable
 fi
+flatpak install --user -y --noninteractive ./repo com.jagex.Launcher
 flatpak build-update-repo ${GPG_ARGS} repo --title="Jagex Launcher" --generate-static-deltas --default-branch=stable --prune
